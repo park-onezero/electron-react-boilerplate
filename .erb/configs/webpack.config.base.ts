cd981f296f47128869873abd06924e1f51733d68
parkonezero <park.onezero@gmail.com>
2023-12-15 17:04:02 +0900

    tailwindcss + shadcnui + alias 설정

--- .erb/configs/webpack.config.base.ts
+++ .erb/configs/webpack.config.base.ts
@@ -6,12 +6,12 @@ import webpack from 'webpack';
 import TsconfigPathsPlugins from 'tsconfig-paths-webpack-plugin';
 import webpackPaths from './webpack.paths';
 import { dependencies as externals } from '../../release/app/package.json';
+import path from 'path';
 
 const configuration: webpack.Configuration = {
   externals: [...Object.keys(externals || {})],
-
   stats: 'errors-only',
-
+  
   module: {
     rules: [
       {
@@ -47,6 +47,8 @@ const configuration: webpack.Configuration = {
     modules: [webpackPaths.srcPath, 'node_modules'],
     // There is no need to add aliases here, the paths in tsconfig get mirrored
     plugins: [new TsconfigPathsPlugins()],
+    alias: {'@':webpackPaths.srcRendererPath},
+    
   },
 
   plugins: [
