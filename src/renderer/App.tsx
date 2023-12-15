8bbb954b12796807af56c3a6912edca8e408b264
parkonezero <park.onezero@gmail.com>
2023-12-15 17:41:00 +0900

    .

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,24 +1,16 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom'; 
-import './global.css';
-import { Button } from '@/components/ui/button';
-
-function Hello() {
-  return (
-    <div> 
-        <Button variant="destructive">Button Test</Button>
-        <div className="text-center border-solid border-4 border-red-500 bg-black text-white shadow">
-          ERB + TAILWIND = ❤
-      </div> 
-    </div>
-  );
-}
+import './global.css'; 
+import Main from './pages/Main';
 
 export default function App() {
   return (
+    <div>
+      <div id="portal"/>
     <Router>
       <Routes>
-        <Route path="/" element={<Hello />} />
+        <Route path="/" element={<Main />} />
       </Routes>
     </Router>
+    </div>
   );
 }
